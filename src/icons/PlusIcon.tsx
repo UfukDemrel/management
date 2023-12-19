9656fa1ffa058bc52a588be85634dcac7d747de9
Ufuk Demirel <udemirel012@gmail.com>
2023-12-19 20:48:33 +0300

    first commit

--- src/icons/PlusIcon.tsx
+++ src/icons/PlusIcon.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function PlusIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width="2rem" height="2rem">
